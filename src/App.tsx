2d8239e61c9cc64906e5378f59834398d6fbd032
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-01-09 21:10:53 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import WelcomePage from "./Component/WelcomePage.tsx";
+import SignIn from "./Component/SignIn.tsx";
+
+function App() {
+
+  return (
+    <>
+        <WelcomePage/>
+        <SignIn/>
+    </>
+  )
+}
+
+export default App
