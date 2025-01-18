31ca18947ec8b89888cf2d3f559ace2ea35488ca
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-01-18 10:43:52 +0530

    Monitoring log done

--- src/Pages/Log.tsx
+++ src/Pages/Log.tsx
@@ -1,7 +1,29 @@
+import LogHeader from "../Component/MonitoringLogComponent/LogHeader.tsx";
+import LogAdd from "../Component/MonitoringLogComponent/LogAdd.tsx";
+import LogTable from "../Component/MonitoringLogComponent/LogTable.tsx";
+
 export default function Log(){
+    const addLogBtn = document.getElementById('addLogBtn')as HTMLButtonElement;
+    const addFormCloseBtn = document.getElementById('closeLogForm')as HTMLButtonElement;
+    const addLogForm = document.getElementById('logFormCard')as HTMLElement;
+
+    if (addLogBtn) {
+        addLogBtn.addEventListener('click', () => {
+            addLogForm.style.display = 'block';
+        })
+    }
+    if (addFormCloseBtn){
+        addFormCloseBtn.addEventListener('click', () => {
+            addLogForm.style.display = 'none';
+        })
+    }
     return(
         <>
-            <h1>Log</h1>
+            <section id="monitory" className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+               <LogHeader/>
+                <LogAdd/>
+                <LogTable/>
+            </section>
         </>
     )
 }
