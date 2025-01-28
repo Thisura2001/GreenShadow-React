ea009386cd3cffb715355cf8b7fa0dd761912add
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-01-28 12:08:10 +0530

    Store created

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,7 +1,21 @@
 import {configureStore} from "@reduxjs/toolkit";
+import CropSlice from "../Reducer/CropSlice.ts";
+import EquipmentSlice from "../Reducer/EquipmentSlice.ts";
+import FiledSlice from "../Reducer/FiledSlice.ts";
+import LogSlice from "../Reducer/LogSlice.ts";
+import SignUpSlice from "../Reducer/SignUpSlice.ts";
+import StaffSlice from "../Reducer/StaffSlice.ts";
+import VehicleSlice from "../Reducer/VehicleSlice.ts";
 
 export const store = configureStore({
     reducer: {
-
+        crops:CropSlice,
+        equipments:EquipmentSlice,
+        fields:FiledSlice,
+        logs:LogSlice,
+        signUp:SignUpSlice,
+        staffs:StaffSlice,
+        vehicles:VehicleSlice,
     }
 });
+export type AppDispatch = typeof store.dispatch;
